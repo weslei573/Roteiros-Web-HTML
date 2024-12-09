5a0b8c0939a21b97d1537273b6dc3748cce7cb87
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2024-12-09 16:57:30 -0400

    Update main.js

--- JavaScript/05_Code-Challenge/js/main.js
+++ JavaScript/05_Code-Challenge/js/main.js
@@ -1,2 +1,17 @@
 // Seu primeiro desafio de código
 
+// Escreva um código que retornará uma letra aleatória do seu nome
+
+// O método charAt() retorna o caractere especificado a partir de uma string.
+console.log("Weslei".charAt());
+
+console.log(Math.floor(Math.random() * 4 + 1));
+console.log(Math.floor(Math.random() * 4 + 1));
+
+console.log("Weslei".charAt(Math.floor(Math.random() * 6)));
+
+// Vamos fazer isso funcionar para todo o nome ||OK||
+const anyName = "Augusto";
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+
+
