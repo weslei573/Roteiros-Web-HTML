c23a481f13a58a835e334cf391272c5392d049bb
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2024-12-11 22:37:26 -0400

    aula de input

--- JavaScript/10_User-input/js/main.js
+++ JavaScript/10_User-input/js/main.js
@@ -1,1 +1,23 @@
 // User Input
+
+// O método Window.alert() mostra uma caixa de diálogo de aviso com o conteúdo opcionalmente especificado e um botão OK.
+// || -> alert("Hello World!"); <-
+
+// O método Window.confirm() mostra uma janela modal com uma mensagem opcional e dois botões, OK e Cancelar.
+// || -> \n e uma quebra de linha
+// || -> confirm("OK === True\nCancel === False"); <- 
+
+// Exemplo de capitura de click ok ou cancel como boolean
+// || -> let myBoolean = confirm("OK === True\nCancel === False"); 
+// || -> console.log(myBoolean); 
+
+// Exemplo de pega nome, e se não coloca nada ele dara um comentario
+let name = prompt("Please enter your name.");
+if (name) {
+    console.log(name.length);
+    console.log(name.trim().length);
+    console.log(name.trim());
+} else {
+    console.log("Your didn't enter your name!");
+    
+}
